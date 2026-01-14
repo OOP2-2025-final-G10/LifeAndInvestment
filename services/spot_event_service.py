e741f129cc1409a7b593e9ba61f2eb748eb515bc
qwertyuiop <121487905+BBBBCDEF@users.noreply.github.com>
2026-01-15 03:13:18 +0900

    マス目ごとのイベントを実装可能に
    spot_event_service.pyのmatch文でマス目に止まった際のイベントを定義できるようにした。
    株の銘柄の名前を変更した。

--- services/spot_event_service.py
+++ services/spot_event_service.py
@@ -4,12 +4,33 @@ class SpotEventService:
 
     @staticmethod
     def handle(user, db):
-        """
-        マス目に止まった後のイベント処理
-        """
         spot = user.spot_id
 
-        #↓マス目ごとのイベント定義
+        stock_names = ["東葉電気", "Novasystems", "関東食品", "南日本旅客鉄道", "林不動産レジデンシャル"]
 
+        #↓マス目ごとのイベント定義
+        #0はスタート地点のため除外
+        #以下は例なので、変更してください
+        #134マス目まで実装してください
+        match spot:
+            #例
+            case 1: #1マス目に止まった場合
+                #所持金を増加させる
+                UserEvent.add_money(user, 5000)
+            case 2: #2マス目に止まった場合
+                #所持金を減少させる
+                UserEvent.subtract_money(user, 5000)
+            case 3: #3マス目に止まった場合
+                #職業を変更する
+                UserEvent.change_job(user, "医者")
+            case 4: #4マス目に止まった場合
+                #給料を支払う
+                UserEvent.give_salary(user)
+            case 5:
+                #株を無理やり購入させる例
+                UserEvent.buy_stock(user, "東葉電気", 10, db)
+            case 6:
+                #株を無理やり売却させる例
+                UserEvent.buy_stock(user, "Novasystems", 10, db)
 
         user.save(db)
